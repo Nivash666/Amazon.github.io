3be99787421771e1242a78fa8b25ade286521ad9
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-03-23 15:48:12 +0530

    debug true

--- amazon/settings.py
+++ amazon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$ybza86rh8wa^66%^&h!8jy2#d4kftk-t91rj!g)c1i1+g1@-b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_COOKIE_SECURE = False
