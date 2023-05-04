8622ba97847d74c942f00d56fc09599bed26e38e
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-05-04 16:30:14 +0530

    again

--- amazon/settings.py
+++ amazon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$ybza86rh8wa^66%^&h!8jy2#d4kftk-t91rj!g)c1i1+g1@-b
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = ['amazonclone.up.railway.app', 'https://amazonclone.up.railway.app/']
+ALLOWED_HOSTS = ['amazonclone.up.railway.app']
 USE_X_FORWARDED_HOST = True
 
 
