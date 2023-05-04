77e1e0e8ec3e057d5c3c7801f4d5ef08255e8888
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-05-04 16:41:58 +0530

    yess

--- amazon/settings.py
+++ amazon/settings.py
@@ -25,10 +25,11 @@ SECRET_KEY = 'django-insecure-$ybza86rh8wa^66%^&h!8jy2#d4kftk-t91rj!g)c1i1+g1@-b
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = ['amazonclone.up.railway.app']
-USE_X_FORWARDED_HOST = True
-
-
+ALLOWED_HOSTS = ['*']
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_DOMAIN = 'amazonclone.up.railway.app'
+CSRF_TRUSTED_ORIGINS = ['https://amazonclone.up.railway.app/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
